63caafba0dae5204195e1d3a687006974c24039f
cihancolak <cihancolak1997@gmail.com>
2024-06-07 22:20:00 +0000

    This project containerize and it works well also set up Kubernetes cluster using Minikube.

--- app.py
+++ app.py
@@ -2,21 +2,27 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-
+# In-memory database
 books = []
 
+# Home route
+@app.route('/')
+def home():
+    return "Welcome to the Book API!"
 
+# Route to get all books
 @app.route('/books', methods=['GET'])
 def get_books():
     return jsonify(books), 200
 
+# Route to add a new book
 @app.route('/books', methods=['POST'])
 def add_book():
     new_book = request.get_json()
     books.append(new_book)
     return jsonify(new_book), 201
 
-
+# Route to get a book by ID
 @app.route('/books/<int:book_id>', methods=['GET'])
 def get_book(book_id):
     book = next((book for book in books if book['id'] == book_id), None)
@@ -25,7 +31,7 @@ def get_book(book_id):
     else:
         return jsonify({'error': 'Book not found'}), 404
 
-
+# Route to update a book by ID
 @app.route('/books/<int:book_id>', methods=['PUT'])
 def update_book(book_id):
     book = next((book for book in books if book['id'] == book_id), None)
@@ -36,7 +42,7 @@ def update_book(book_id):
     else:
         return jsonify({'error': 'Book not found'}), 404
 
-
+# Route to delete a book by ID
 @app.route('/books/<int:book_id>', methods=['DELETE'])
 def delete_book(book_id):
     global books
@@ -45,3 +51,4 @@ def delete_book(book_id):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
